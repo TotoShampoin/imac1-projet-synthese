1e3110ae7b5d4609418c792e4c79961984ded776
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-04-25 15:37:04 +0200

    added 3d vector

---
+++ include/vec3.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Vec3f {
+    float x, y, z;
+
+    Vec3f(float x, float y, float z);
+
+    Vec3f& operator=(Vec3f other);
+    bool operator==(Vec3f other);
+    Vec3f operator-();
+    Vec3f operator+(Vec3f other);
+    Vec3f operator-(Vec3f other);
+    Vec3f operator*(float other);
+    Vec3f operator/(float other);
+    float operator*(Vec3f other);
+
+    float norm();
+    Vec3f normalize();
+};
---
+++ src/vec3.cpp
@@ -0,0 +1,66 @@
+#include "vec3.h"
+#include <cmath>
+
+Vec3f::Vec3f(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vec3f& Vec3f::operator=(Vec3f other) {
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+    return (*this);
+}
+bool Vec3f::operator==(Vec3f other) {
+    return 
+        this->x == other.x &&
+        this->y == other.y &&
+        this->z == other.z;
+}
+
+Vec3f Vec3f::operator-() {
+    return Vec3f {
+        -this->x,
+        -this->y,
+        -this->z,
+    };
+}
+Vec3f Vec3f::operator+(Vec3f other) {
+    return Vec3f {
+        this->x + other.x,
+        this->y + other.y,
+        this->z + other.z,
+    };
+}
+Vec3f Vec3f::operator-(Vec3f other) {
+    return Vec3f {
+        this->x - other.x,
+        this->y - other.y,
+        this->z - other.z,
+    };
+}
+Vec3f Vec3f::operator*(float other) {
+    return Vec3f {
+        this->x * other,
+        this->y * other,
+        this->z * other,
+    };
+}
+Vec3f Vec3f::operator/(float other) {
+    return Vec3f {
+        this->x / other,
+        this->y / other,
+        this->z / other,
+    };
+}
+float Vec3f::operator*(Vec3f other) {
+    return this->x * other.x + this->y * other.y + this->z * other.z;
+}
+float Vec3f::norm() {
+    return sqrt((*this) * (*this));
+}
+Vec3f Vec3f::normalize() {
+    return (*this) / this->norm();
+}
