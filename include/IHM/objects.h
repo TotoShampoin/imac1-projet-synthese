453ba640e306ccfa716828414758014215531519
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-24 12:55:09 +0200

    dumb

--- include/IHM/objects.h
+++ include/IHM/objects.h
@@ -13,6 +13,7 @@ struct Image {
 struct Geometry {
     unsigned long vert_nb;
     GLfloat* vertices;  // * 3
+    GLfloat* uv;        // * 2
     GLfloat* colors;    // * 4
     unsigned long tri_nb;
     GLuint* triangles;  // * 3
