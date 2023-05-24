212b02d5a579b64b34bf30f8401953e1d566a1b3
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-24 12:49:37 +0200

    added bonus (not tested)

--- include/game/bonus.h
+++ include/game/bonus.h
@@ -1,22 +1,44 @@
 #pragma once
-#include <vector>
+#include <map>
 #include <functional>
 
 #include "logic/vec3.h"
 #include "IHM/objects.h"
 
+#include "game/ball.h"
+#include "game/player.h"
+#include "game/racket.h"
+
 struct Type {
-    Geometry& forme;
-    std::function<void ()> start;
-    std::function<void ()> end;
+    std::function<void (Player&)> start;
+    std::function<void (Player&)> end;
+    float time_in_second = 10.;
+    Vec3f size {.25, .25, .25};
 };
 
 struct Bonus {
-    Vec3f position;
     Type& specs;
+    Vec3f position;
+    float time_remaining;
+
+    bool is_active = false;
+    bool is_picked = false;
+    Player* player = nullptr;
+
+    void update(float delta_time);
+
+    void start();
+    void end();
+    void bePicked(Player&);
 };
 
-struct TypeTable {
-    std::vector<Type> types;
-    Bonus createBonus(int type_id, Vec3f position);
+enum TypeID {
+    VICTORY,
+    SLOW_BALL,
+    LARGE_RACKET,
 };
+static std::vector<Type> type_table;
+
+void initTypeTable();
+
+Bonus createBonus(TypeID type_id, Vec3f position);
--- src/game/bonus.cpp
+++ src/game/bonus.cpp
@@ -1,6 +1,59 @@
 #include "game/bonus.h"
+#include "IHM/objects.h"
 
-Bonus TypeTable::createBonus(int type_id, Vec3f position) {
-    Type& specs = types[type_id];
-    return Bonus {position, specs};
+void initTypeTable() {
+    type_table[VICTORY] = Type {
+        [](Player &P) {
+            P.ball.speed /= 2;
+        },
+        [](Player &P) {
+            P.ball.speed *= 2;
+        },
+        0, Vec3f(2, 2, .5)
+    };
+    type_table[SLOW_BALL] = Type {
+        [](Player &P) {
+            P.ball.speed /= 2;
+        },
+        [](Player &P) {
+            P.ball.speed *= 2;
+        }
+    };
+    type_table[LARGE_RACKET] = Type {
+        [](Player &P) {
+            P.racket.scale *= 1.5;
+        },
+        [](Player &P) {
+            P.racket.scale /= 1.5;
+        }
+    };
+}
+
+Bonus createBonus(int type_id, Vec3f position) {
+    Type& sp = type_table[type_id];
+    return Bonus {sp, position, sp.time_in_second};
+}
+
+void Bonus::update(float delta_time) {
+    if(is_active) {
+        time_remaining -= delta_time;
+        if(time_remaining <= 0) {
+            end();
+        }
+    }
+}
+
+void Bonus::start() {
+    specs.start(*player);
+    is_active = true;
+}
+void Bonus::end() {
+    specs.end(*player);
+    player = nullptr;
+    is_active = false;
+}
+void Bonus::bePicked(Player& P) {
+    player = &P;
+    is_picked = true;
+    start();
 }
