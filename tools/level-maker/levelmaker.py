c949d426105f574ab04c27ecbf12b4656da763d0
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-29 19:00:21 +0200

    added a tool to make levels (very bad)

---
+++ tools/level-maker/levelmaker.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+from sys import argv
+from parse import parse
+import ctypes
+
+length = 0
+width = 0
+height = 0
+obstacles = []
+bonuses = []
+
+with open(argv[1], "r") as file:
+    for _line in file.readlines():
+        line = _line.replace("\n", "")
+        if line.startswith("l "):
+            length, width, height = parse("l {} {} {}", line)
+            length, width, height = float(length), float(width), float(height)
+        if line.startswith("o "):
+            res = parse("o {} {} {} {} {} {}", line)
+            x1 , y1 , z1 , x2 , y2 , z2 = res
+            x1 , y1 , z1 = float(x1) , float(y1) , float(z1)
+            x2 , y2 , z2 = float(x2) , float(y2) , float(z2)
+            obstacles.append({
+                "boundA": (x1, y1, z1),
+                "boundB": (x2, y2, z2)
+            })
+        if line.startswith("b "):
+            type , x , y , z = parse("b {} {} {} {}", line)
+            x , y , z = float(x) , float(y) , float(z)
+            type = int(type)
+            bonuses.append([{
+                "type": type,
+                "coord": (x, y, z)
+            }])
+
+print(length, len(obstacles), len(bonuses))
+
+with open(argv[2], "wb") as file:
+    file.write(ctypes.c_float(length))
+    file.write(ctypes.c_float(width))
+    file.write(ctypes.c_float(height))
+    file.write(ctypes.c_ulong(len(obstacles)))
+    file.write(ctypes.c_ulong(len(bonuses)))
+    for obs in obstacles:
+        file.write(ctypes.c_float(obs["boundA"][0]))
+        file.write(ctypes.c_float(obs["boundA"][1]))
+        file.write(ctypes.c_float(obs["boundA"][2]))
+        file.write(ctypes.c_float(obs["boundB"][0]))
+        file.write(ctypes.c_float(obs["boundB"][1]))
+        file.write(ctypes.c_float(obs["boundB"][2]))
+    for bon in bonuses:
+        file.write(ctypes.c_uint(bon["type"]))
+        file.write(ctypes.c_float(bon["coord"][0]))
+        file.write(ctypes.c_float(bon["coord"][1]))
+        file.write(ctypes.c_float(bon["coord"][2]))
