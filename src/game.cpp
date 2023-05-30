2ea5276515ae28cd714a01e1d9118b2749645e76
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-30 22:37:45 +0200

    added pause

--- src/game.cpp
+++ src/game.cpp
@@ -11,19 +11,94 @@
 struct Game {
     Player player;
     Level level;
-    bool is_pause;
-    bool pause_switched;
+    Window& window;
+    bool is_pause = false;
+    bool pause_switched = false;
+    double timer = 0;
+
+    Game(Player pl, Level lv, Window& wn): 
+        player(pl),
+        level(lv),
+        window(wn)
+    {
+        makeGameEvents();
+    }
+
+    void makeGameEvents() {
+        Game& game = *this;
+        Level& level = this->level;
+        Player& player = this->player;
+        Window& win = window;
+
+        win.on_mouse_move = [&player, &win, &level](double xpos, double ypos) {
+            float lim_x = level.width - player.racket.scale.x;
+            float lim_y = level.height - player.racket.scale.y;
+            player.setPosition(
+                clamp(xpos, -lim_x, lim_x), 
+                clamp(ypos, -lim_y, lim_y)
+            );
+        };
+        win.on_mouse_button = [&player](int button, int action, int mods) {
+            if(action == GLFW_PRESS) {
+                switch(button) {
+                case GLFW_MOUSE_BUTTON_LEFT: {
+                    player.launchBall();
+                } break;
+                }
+            }
+        };
+        win.on_key = [&player, &game](int key, int scancode, int action, int mods) {
+            if(action == GLFW_PRESS) {
+                switch(key) {
+                case GLFW_KEY_W: {
+                    player.racket.isMovingForward = !player.racket.hasBall;
+                } break;
+                case GLFW_KEY_ESCAPE: {
+                    game.pause();
+                } break;
+                }
+            }
+            if(action == GLFW_RELEASE) {
+                switch(key) {
+                case GLFW_KEY_W: {
+                    player.racket.isMovingForward = false;
+                } break;
+                }
+            }
+        };
+    }
+    void makePauseEvents() {
+        Game& game = *this;
+        Window& win = window;
+
+        win.on_mouse_move = [](double xpos, double ypos) {};
+        win.on_mouse_button = [](int button, int action, int mods) {};
+        win.on_key = [&game](int key, int scancode, int action, int mods) {
+            if(action == GLFW_PRESS) {
+                switch(key) {
+                case GLFW_KEY_ESCAPE: {
+                    game.resume();
+                } break;
+                }
+            }
+        };
+    }
 
     void pause() {
         is_pause = true;
         pause_switched = true;
+        makePauseEvents();
     }
     void resume() {
         is_pause = false;
         pause_switched = true;
+        makeGameEvents();
     }
-    void update() {
+    void update(double delta_time) {
         pause_switched = false;
+        if(!is_pause) {
+            timer += delta_time;
+        }
     }
 };
 
@@ -50,8 +125,6 @@ void display(Window& win, Game& game, double delta_time) {
     static Mesh wall_mesh = makeWallMesh();
     static Geometry racket_mesh = makeRacketMesh();
     static Geometry bonus_mesh = createSphere(4);
-    static double timer = 0;
-    timer += delta_time;
 
     Level& level = game.level;
     Player& player = game.player;
@@ -100,7 +173,17 @@ void display(Window& win, Game& game, double delta_time) {
         if(bonus.specs->is_victory || bonus.is_picked) {
             continue;
         }
-        draw3DObject(bonus_mesh, bonus.position, bonus.specs->size / 2, Vec3f(0, 1, 1), timer * 2 * 180 / M_PI);
+        draw3DObject(bonus_mesh, bonus.position, bonus.specs->size / 2, Vec3f(0, 1, 1), game.timer * 2 * 180 / M_PI);
+    }
+
+    //
+
+    use2dMode(win);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    
+    if(game.is_pause) {
+        glColor4f(0, 0, 0, .5);
+        draw2DBox(Vec2f(0, 0), Vec2f(win.aspect_ratio, 1));
     }
     
     win.refresh();
@@ -110,6 +193,10 @@ void display(Window& win, Game& game, double delta_time) {
 void audio(Game& game, double delta_time) {
     static AudioContext channel;
     static AudioMedia tac ("assets/sounds/switch.wav");
+
+    if(game.is_pause) {
+        return;
+    }
 
     Level& level = game.level;
     Player& player = game.player;
@@ -128,52 +215,15 @@ void gameLoop(Window& win, Game& game) {
     Player& player = game.player;
     
     win.pollEvents();
-    physics(game, delta_time);
-    
+    if(!game.is_pause) {
+        physics(game, delta_time);
+    }
     display(win, game, delta_time);
     audio(game, delta_time);
 
+    game.update(delta_time);
+
     delta_time = timer();
-}
-
-
-void makeEvents(Window& win, Game& game) {
-    Level& level = game.level;
-    Player& player = game.player;
-
-    win.on_mouse_move = [&player, &win, &level](double xpos, double ypos) {
-        float lim_x = level.width - player.racket.scale.x;
-        float lim_y = level.height - player.racket.scale.y;
-        player.setPosition(
-            clamp(xpos, -lim_x, lim_x), 
-            clamp(ypos, -lim_y, lim_y)
-        );
-    };
-    win.on_mouse_button = [&player](int button, int action, int mods) {
-        if(action == GLFW_PRESS) {
-            switch(button) {
-            case GLFW_MOUSE_BUTTON_LEFT: {
-                player.launchBall();
-            } break;
-            }
-        }
-    };
-    win.on_key = [&player](int key, int scancode, int action, int mods) {
-        if(action == GLFW_PRESS) {
-            switch(key) {
-            case GLFW_KEY_W: {
-                player.racket.isMovingForward = !player.racket.hasBall;
-            } break;
-            }
-        }
-        if(action == GLFW_RELEASE) {
-            switch(key) {
-            case GLFW_KEY_W: {
-                player.racket.isMovingForward = false;
-            } break;
-            }
-        }
-    };
 }
 
 
@@ -182,10 +232,9 @@ void startGame(const char* level_path, Window& win) {
 
     Game game {
         Player(),
-        Level(level_path)
+        Level(level_path),
+        win
     };
-
-    makeEvents(win, game);
 
     while(!win.shouldClose()) {
         gameLoop(win, game);
