cc8bb97561c3515006337ab52b7fa3b65c0b9eb3
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-30 19:44:26 +0200

    added coordinate systems

--- include/IHM/draw.h
+++ include/IHM/draw.h
@@ -2,14 +2,31 @@
 
 #include "IHM/window.h"
 #include "IHM/objects.h"
+#include "logic/vec2.h"
 #include "logic/vec3.h"
+
+struct Coord3D {
+    Vec3f position;
+    Vec3f scale;
+    Vec3f rotation_axis;
+    float rotation_angle;
+};
+struct Coord2D {
+    Vec2f position;
+    Vec2f scale;
+    float rotation_angle;
+};
 
 const double GL_VIEW_SIZE = 2.;
 
-void draw3DObject(Geometry& geo, Image& img, Vec3f vec = Vec3f(0, 0, 0), Vec3f scale = Vec3f(1, 1, 1));
-void draw3DObject(Geometry& geo, Vec3f vec = Vec3f(0, 0, 0), Vec3f scale = Vec3f(1, 1, 1));
-void draw2DTexture(Image& img, GLdouble pos_x, GLdouble pos_y, GLdouble scale = 1.);
-void draw2DTexture(Image& img, GLdouble pos_x, GLdouble pos_y, GLdouble scale_x, GLdouble scale_y);
+void draw3DObject(Geometry& geo, Image& img, Coord3D coords);
+void draw3DObject(Geometry& geo, Coord3D coords);
+void draw2DTexture(Image& img, Coord2D coords);
+
+void draw3DObject(Geometry& geo, Image& img, Vec3f vec = Vec3f(0, 0, 0), Vec3f scale = Vec3f(1, 1, 1), Vec3f rotation_axis = Vec3f(1, 0, 0), float rotation_angle = 0);
+void draw3DObject(Geometry& geo, Vec3f vec = Vec3f(0, 0, 0), Vec3f scale = Vec3f(1, 1, 1), Vec3f rotation_axis = Vec3f(1, 0, 0), float rotation_angle = 0);
+void draw2DTexture(Image& img, Vec2f pos, GLdouble scale = 1., GLdouble rotation = 0.);
+void draw2DTexture(Image& img, Vec2f pos, Vec2f scale = Vec2f(1, 1), GLdouble rotation = 0.);
 void drawGeometry(Geometry& geo);
 void drawSquare(Image& img);
 
--- src/IHM/draw.cpp
+++ src/IHM/draw.cpp
@@ -3,40 +3,53 @@
 
 #include <cmath>
 
-void draw3DObject(Geometry& geo, Image& img, Vec3f vec, Vec3f scale) {
+void draw3DObject(Geometry& geo, Image& img, Coord3D& coords) {
+    draw3DObject(geo, img, coords.position, coords.scale, coords.rotation_axis, coords.rotation_angle);
+}
+void draw3DObject(Geometry& geo, Coord3D& coords) {
+    draw3DObject(geo, coords.position, coords.scale, coords.rotation_axis, coords.rotation_angle);
+}
+void draw2DTexture(Image& img, Coord2D& coords) {
+    draw2DTexture(img, coords.position, coords.scale, coords.rotation_angle);
+}
+
+void draw3DObject(Geometry& geo, Image& img, Vec3f vec, Vec3f scale, Vec3f rotation_axis = Vec3f(1, 0, 0), float rotation_angle = 0) {
     glEnable(GL_TEXTURE_2D);
     glEnable(GL_BLEND);
     glBindTexture(GL_TEXTURE_2D, img.texture_id);
-    draw3DObject(geo, vec, scale);
+    draw3DObject(geo, vec, scale, rotation_axis, rotation_angle);
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_BLEND);
     glDisable(GL_TEXTURE_2D);
 }
 
-void draw3DObject(Geometry& geo, Vec3f vec, Vec3f scale) {
+void draw3DObject(Geometry& geo, Vec3f vec, Vec3f scale, Vec3f rotation_axis = Vec3f(1, 0, 0), float rotation_angle = 0) {
     glPushMatrix();
         glTranslated(vec.x, vec.y, vec.z);
         glScaled(scale.x, scale.y, scale.z);
+        glRotated(rotation_angle, rotation_axis.x, rotation_axis.y, rotation_axis.z);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         drawGeometry(geo);
     glPopMatrix();
 }
 
-void draw2DTexture(Image& img, GLdouble pos_x, GLdouble pos_y, GLdouble scale) {
+void draw2DTexture(Image& img, Vec2f pos, GLdouble scale, GLdouble rotation) {
     glPushMatrix();
-        glTranslated(pos_x, pos_y, 0);
+        glTranslated(pos.x, pos.y, 0);
         glScaled(scale, scale, 1);
         glScalef(1, (float)img.height/img.width, 1);
+        glRotated(rotation, 0, 0, 1);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         drawSquare(img);
     glPopMatrix();
 }
 
-void draw2DTexture(Image& img, GLdouble pos_x, GLdouble pos_y, GLdouble scale_x, GLdouble scale_y) {
+void draw2DTexture(Image& img, Vec2f pos, Vec2f scale, GLdouble rotation) {
     glPushMatrix();
-        glTranslated(pos_x, pos_y, 0);
-        glScaled(scale_x, scale_y, 1);
+        glTranslated(pos.x, pos.y, 0);
+        glScaled(scale.x, scale.y, 1);
         glScalef(1, (float)img.height/img.width, 1);
+        glRotated(rotation, 0, 0, 1);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         drawSquare(img);
     glPopMatrix();
