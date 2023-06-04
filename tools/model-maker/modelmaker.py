5e8a6b8dc602d46396b26cbe115b39cb1b1dc393
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-06-04 13:04:14 +0200

    fixed ulong being 32bit somehow...

--- tools/model-maker/modelmaker.py
+++ tools/model-maker/modelmaker.py
@@ -20,13 +20,13 @@ for t in range(len(scene.vertices)*2):
     tex_coords += [0]
 
 with open(argv[2], "wb") as file:
-    file.write(ctypes.c_ulong(len(vertices)//3))
-    file.write(ctypes.c_ulong(len(faces)//3))
+    file.write(ctypes.c_uint64(len(vertices)//3))
+    file.write(ctypes.c_uint64(len(faces)//3))
     print(len(vertices)//3, len(faces)//3)
     for v in vertices:
         file.write(ctypes.c_float(v))
     for t in tex_coords:
         file.write(ctypes.c_float(v))
     for f in faces:
-        file.write(ctypes.c_uint(f))
+        file.write(ctypes.c_uint32(f))
 
