64d155aca9d8f4d5d9f3de4cb9130ba193705c21
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-06-04 16:24:37 +0200

    fixed allocation

--- src/draw-game.cpp
+++ src/draw-game.cpp
@@ -17,6 +17,21 @@ void setColor(Geometry& geo, float r, float g, float b, float a) {
     }
 }
 
+// Broken ?
+void vertexLight(Geometry& geo, Vec3f position, float dist_max, float r, float g, float b) {
+    GLfloat* colors = geo.colors;
+    for(int i = 0; i < geo.vert_nb; i++) {
+        int ic = i * 3;
+        Vec3f vert {
+            geo.vertices[ic+0],
+            geo.vertices[ic+1],
+            geo.vertices[ic+2]
+        };
+        float light = 1 - (vert * position) / dist_max;
+        set_coord(geo.colors, i, r * light, g * light, b * light, 1);
+    }
+}
+
 void drawBall(Ball& ball) {
     static Mesh ball_mesh = makeBallMesh();
 
@@ -57,7 +72,7 @@ void drawLevel(Level& level, Player& player, double time) {
         if(bonus.specs->is_victory || bonus.is_picked) {
             continue;
         }
-        draw3DObject(bonus.specs->mesh, bonus.position, bonus.specs->size / 2, Vec3f(0, 1, 1), time * 2 * 180 / M_PI);
+        draw3DObject(*bonus.specs->mesh, bonus.position, bonus.specs->size / 2, Vec3f(0, 1, 0), time * 2 * 180 / M_PI);
     }
 }
 
